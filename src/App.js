6baf9dc4417a8943a0dbc4bf9ec3be28a1be4127
Andrew <andrew.mk4@yahoo.com>
2021-11-03 23:56:48 -0700

    burger open close x

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import * as React from 'react';
+import { useState } from "react";
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './global';
 import { theme } from './theme';
 import { Burger, Menu } from "./components";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyles />
         <div>
-          <Burger />
-          <Menu />
-        </div>
-        <div>
           <h1>Hello. This is burger menu tutorial</h1>
           <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
           <small>Icon made by Freepik from www.flaticon.com</small>
+        </div>
+        <div>
+          <Burger open={open} setOpen={setOpen} />
+          <Menu open={open} setOpen={setOpen} />
         </div>
       </>
     </ThemeProvider>
